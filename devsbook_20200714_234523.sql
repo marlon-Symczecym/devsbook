7e520031248c0f9218fe3829dc4a15d1ac46fdfa
marlon-symczecym <marlonsymczecym.dev@gmail.com>
2020-07-14 23:45:37 -0300

    database

---
+++ devsbook_20200714_234523.sql
@@ -0,0 +1,97 @@
+-- Valentina Studio --
+-- MySQL dump --
+-- ---------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- ---------------------------------------------------------
+
+
+-- CREATE TABLE "posts" ----------------------------------------
+CREATE TABLE `posts`( 
+	`id` Int( 255 ) AUTO_INCREMENT NOT NULL,
+	`type` VarChar( 20 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+	`created_at` DateTime NOT NULL,
+	`body` Text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+	`id_user` Int( 10 ) NOT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 14;
+-- -------------------------------------------------------------
+
+
+-- CREATE TABLE "postcomments" ---------------------------------
+CREATE TABLE `postcomments`( 
+	`id` Int( 255 ) NOT NULL,
+	`id_post` Int( 255 ) NOT NULL,
+	`id_user` Int( 255 ) NOT NULL,
+	`created_at` DateTime NOT NULL,
+	`body` Text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci
+ENGINE = InnoDB;
+-- -------------------------------------------------------------
+
+
+-- CREATE TABLE "postlikes" ------------------------------------
+CREATE TABLE `postlikes`( 
+	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+	`id_post` Int( 255 ) NOT NULL,
+	`id_user` Int( 255 ) NOT NULL,
+	`created_at` DateTime NOT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 9;
+-- -------------------------------------------------------------
+
+
+-- CREATE TABLE "userrelations" --------------------------------
+CREATE TABLE `userrelations`( 
+	`id` Int( 255 ) AUTO_INCREMENT NOT NULL,
+	`user_from` Int( 11 ) NOT NULL,
+	`user_to` Int( 11 ) NOT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 10;
+-- -------------------------------------------------------------
+
+
+-- CREATE TABLE "users" ----------------------------------------
+CREATE TABLE `users`( 
+	`id` Int( 255 ) AUTO_INCREMENT NOT NULL,
+	`email` VarChar( 100 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+	`password` VarChar( 200 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+	`name` VarChar( 100 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+	`birthdate` Date NOT NULL,
+	`city` VarChar( 100 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+	`work` VarChar( 100 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+	`avatar` VarChar( 100 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'default.jpg',
+	`cover` VarChar( 100 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'cover.jpg',
+	`token` VarChar( 200 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 6;
+-- -------------------------------------------------------------
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ---------------------------------------------------------
+
+
